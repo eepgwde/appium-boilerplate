27dd29c147c30fce1ce302020960dd149860a4de
Walter Eaves <walti@walti.mooo.com>
2021-12-28 20:44:54 +0100

    usable

--- tests/specs3/app.login.spec.ts
+++ tests/specs3/app.login.spec.ts
@@ -1,25 +1,29 @@
-import TabBar from '../screenobjects/components/TabBar';
 import LoginScreen from '../screenobjects/LoginScreen';
-import NativeAlert from '../screenobjects/components/NativeAlert';
 import AppScreen from "../screenobjects/AppScreen";
 import ConsentScreen from "../screenobjects/ConsentScreen";
+import HomeScreen from "../screenobjects/HomeScreen";
 
-import {Screens} from "../screenobjects/NewPage";
-import NewPage = Screens.NewPage;
+// weaves
+// Only successful if run from fullReset.
 
-describe('Signatures for pages,', () => {
+// Illustrates the addition of commands to browser in AppScreen.
+// Drops in the debugger at the end of the login.
+// You should attach an Inspector and record what you do.
+
+const slow0 = 8000;
+const slow1 = 3*slow0;
+
+describe('One session only - login and drop into debugger: ', () => {
     beforeEach(async () => {
-        AppScreen.browser = browser; // set the browser for the page dumper and add functions
+        AppScreen.browser = browser; // set the browser for the page dumper and add commands
 
-        // Added by the above
-        const v0 = await browser.actions0("username")
-        console.log("actions0: " + JSON.stringify(v0));
+        // a moment to stabilize
+        browser.pause(slow0)
+        await browser.getSignature("startup");
 
-        await NewPage.getSignature();
-
-        const t0 = await ConsentScreen.waitForConsent(); // just in case it appears
-        if (t0) console.log('signature: ' + await NewPage.getSignature());
+        const t0 = await ConsentScreen.waitForIsShown(true, slow0); // Consent has an override
         await LoginScreen.waitForIsShown(true);
+        console.log('signature: ' + await browser.getSignature("login"));
     });
 
     it('should be able login successfully', async () => {
@@ -28,39 +32,14 @@ describe('Signatures for pages,', () => {
         // Submit the data
         await LoginScreen.submitLoginForm1({username: 'o2udo00000002' + '\n', password: 'test' + '\n'});
 
-        await LoginScreen.waitForIsShown(false);
-        await NewPage.getSignature();
+        // Logging in takes a long time
+        await LoginScreen.waitForIsShown(false, slow1);
+
+        // And the homescreen should appear
+        await HomeScreen.waitForIsShown(true, slow1);
+        console.log('signature: ' + await browser.getSignature("home"));
 
         await browser.debug()
-    });
-
-});
-
-describe('Signatures 2 for pages,', () => {
-    beforeEach(async () => {
-        AppScreen.browser = browser; // set the browser for the page dumper and add functions
-
-        // Added by the above
-        const v0 = await browser.actions0("username")
-        console.log("actions0: " + JSON.stringify(v0));
-
-        await NewPage.getSignature();
-
-        const t0 = await ConsentScreen.waitForConsent(); // just in case it appears
-        if (t0) console.log('signature: ' + await NewPage.getSignature());
-        await LoginScreen.waitForIsShown(true);
-    });
-
-    it('should be able login successfully', async () => {
-        // Always make sure you are on the right tab
-        await LoginScreen.radioButton('MOCK');
-        // Submit the data
-        await LoginScreen.submitLoginForm1({username: 'o2udo00000002' + '\n', password: 'test' + '\n'});
-
-        await LoginScreen.waitForIsShown(false);
-        await NewPage.getSignature();
-
-        // await browser.debug()
     });
 
 });
