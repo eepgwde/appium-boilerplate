ca31e541bc76f60cf00115ffe12d721b252db199
Walter Eaves <walti@walti.mooo.com>
2021-12-31 15:37:21 +0100

    properties still too many buttons

--- tests/helpers/bak/Actions2.mjs
+++ tests/helpers/bak/Actions2.mjs
@@ -14,7 +14,10 @@ const files1 = files.filter(fn => fn.endsWith('_desc.properties')).map( fn => pa
 
 console.log(files1[0])
 
-var properties = PropertiesReader(files1[0])
+// I have 12 buttons, it is only six from $$()
+const ch0 = path.join(ddir, 'w509bd21d_desc.properties')
+
+var properties = PropertiesReader(ch0)
 
 console.log(properties.get("hashCode"))
 console.log(properties.length)
@@ -52,32 +55,40 @@ const props = properties.getAllProperties()
 const keys = Object.keys(props)
 
 const kvs = keys.filter(k => k.split(".").length > 3).map( k => [k.split(".")[2], k] )
+
+const kvs10 = kvs.map( x => [x[0], x[1], x[1].split(".")[1]] ).filter( x => (x[2] == "text" || x[2] == "desc") ).map(x => [ x[0], x[1] ] ).map( x => [ x[0], properties.get(x[1]) ] )
+
 const kvs1 = kvs.map( x => [x[0], x[1], x[1].split(".")[1]] ).filter( x => (x[2] == "text" || x[2] == "desc") ).map(x => [ x[0], x[1] ] ).map( x => [ x[0], properties.get(x[1]) ] ).filter( x => x[1].length > 0)
 
-const kvs2 = kvs1.sort(function(a, b) {
+console.log('kvs')
+console.log(kvs)
+
+console.log('kvs10')
+console.log(kvs10)
+
+const kvs2 = kvs10.sort(function(a, b) {
     return a[0].localeCompare(b[0])
 });
 
-const kvs3 = Object.fromEntries(kvs2)
-const kvs4 = new Map(Object.entries(kvs3))
+
+console.log(kvs2)
+
+function groupBy(arr, idx) {
+    return arr.reduce(function(acc, obj) {
+	let key=obj[idx]
+	if (!acc[key]) {
+	    acc[key] = []
+	}
+	acc[key].push(obj)
+	return acc
+    }, {})
+}
+
+const v0 = groupBy(kvs2, 0)
+
+const kvs4 = new Map(Object.entries(v0))
 
 console.log(kvs4)
 
-
-const idxes1 = [ ...Array(sections.length).keys() ]
-const sections1 = idxes1.map( i => [ sections[i], (i+1).toString().padStart(2, '0') ])
-const sections2 = Object.fromEntries(sections1)
-const sections3 = new Map(Object.entries(sections2))
-
-console.log(sections3)
-
-const map0 = new Map()
-for (const [key, value] of sections3) {
-    map0.set(key, value.concat(" : " + kvs4.get(key)) )
-}
-
-console.log(map0)
-
-console.log("--hello")
 
 throw '';
